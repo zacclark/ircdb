861d03a42846ab2305856951b4bb8b9d04dc2ef6
Ryan Morrison <ryan.morrison04@gmail.com>
2011-07-28 20:44:55 -0400

    Created Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ircdb::Application.config.secret_token = 'ba679c639f169321e960b38e2d732e4e6c5fcacc0944dfee569b45986b1a17032dc969ce1db50142cfae52e8ced81e1f20e40e11e9b987e0b1ead797e1f9d512'
